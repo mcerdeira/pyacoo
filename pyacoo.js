070fbf3b9b06cdb7858d32e4ea03d6a1421a2120
Martin Cerdeira <martincerdeira@gmail.com>
2012-06-02 12:38:28 -0300

    isalnum added

--- pyacoo.js
+++ pyacoo.js
@@ -1,11 +1,11 @@
 /*
    Python and other cool stuff too JavaScript library
-   
+
    __author__ = 'mRt (martincerdeira@gmail.com)'
    __version__ = '0.01'
    __date__ = '$Date: 2012/05/30$'
-   __license__ = 'GPL v3'   
-      
+   __license__ = 'GPL v3'
+
 */
 
 ///////////////////////////////////
@@ -16,7 +16,7 @@
 
 Function.prototype.method = function (name, func) {
     this.prototype[name] = func;
-    return this;    
+    return this;
 };
 
 ///////////////////////////////////
@@ -61,22 +61,22 @@ String.method('capitalize', function(){ // Pythonic
     return this.charAt(0).toUpperCase() + this.slice(1);
 });
 
-String.method('repeat', function(num){ // Just cool 
-    return new Array(parseInt(num,10) + 1).join(this); 
-}); 
+String.method('repeat', function(num){ // Just cool
+    return new Array(parseInt(num,10) + 1).join(this);
+});
 
 String.method('center', function(width, fillchar){ // Pythonic
     if(width === undefined || width <= this.length){
         return this;
-    }        
+    }
     if(fillchar === undefined){
         fillchar = ' ';
     }
     width = width - this.length;
-    return fillchar.repeat(width - parseInt(width/2,10)) + this + fillchar.repeat(parseInt(width/2,10)) ;    
+    return fillchar.repeat(width - parseInt(width/2,10)) + this + fillchar.repeat(parseInt(width/2,10)) ;
 });
 
-String.method('count', function(sub, start, end){
+String.method('count', function(sub, start, end){ // Pythonic
     if(sub === undefined || sub == ""){
         return this.length + 1;
     }
@@ -84,20 +84,20 @@ String.method('count', function(sub, start, end){
         start = 0;
     }
     if(end === undefined){
-        end = this.length; 
-    }            
-    var n = 0; 
-    var pos = 0; 
-    var str = this.slice(start, end);    
-    while(true){ 
-        pos=str.indexOf(sub,pos); 
-        if(pos!=-1){ n++; pos+=sub.length;} 
-        else{break;} 
-    } 
-    return(n);     
+        end = this.length;
+    }
+    var n = 0;
+    var pos = 0;
+    var str = this.slice(start, end);
+    while(true){
+        pos=str.indexOf(sub,pos);
+        if(pos!=-1){ n++; pos+=sub.length;}
+        else{break;}
+    }
+    return(n);
 });
 
-String.method('endswith', function(suffix, start, end){
+String.method('endswith', function(suffix, start, end){ // Pythonic
     if(suffix === undefined || suffix == ""){
         return true;
     }
@@ -105,23 +105,32 @@ String.method('endswith', function(suffix, start, end){
         start = 0;
     }
     if(end === undefined){
-        end = this.length; 
-    }        
-    var str = this.slice(start, end);    
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;    
+        end = this.length;
+    }
+    var str = this.slice(start, end);
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
 });
 
-String.method('expandtabs', function(tabsize){
+String.method('expandtabs', function(tabsize){ // Pythonic
     if(tabsize === undefined){
-		tabsize = 8;
-	}	
-	return this.replace(String.fromCharCode(9), ' '.repeat(tabsize));	
+        tabsize = 8;
+    }
+    return this.replace(String.fromCharCode(9), ' '.repeat(tabsize));
 });
+
+String.method('isalnum', function(){ // Pythonic
+    if(this == ""){
+        return false;
+    }
+    return (this.search(/[^a-zA-Z0-9]/) == -1);
+});
+
+
 
 /*
-'find', 'format', 'index', 'isalnum', 
+'find', 'format', 'index',
 'isalpha', 'isdigit', 'islower', 'isspace', 'istitle', 'isupper',
-'join', 'ljust', 'lower', 'lstrip', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 
+'join', 'ljust', 'lower', 'lstrip', 'partition', 'replace', 'rfind', 'rindex', 'rjust',
 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
 */
 
