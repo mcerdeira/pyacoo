fa61ee302d5ff2d0b0d95917670c6e2e70a590f0
Martin Cerdeira <martincerdeira@gmail.com>
2012-06-02 12:16:59 -0300

    expandtabs added

--- pyacoo.js
+++ pyacoo.js
@@ -15,8 +15,8 @@
 ///////////////////////////////////
 
 Function.prototype.method = function (name, func) {
-	this.prototype[name] = func;
-	return this;	
+    this.prototype[name] = func;
+    return this;    
 };
 
 ///////////////////////////////////
@@ -26,35 +26,35 @@ Function.prototype.method = function (name, func) {
 ///////////////////////////////////
 
 String.method('trim', function ( ) {  // VB like
-	return this.replace(/^\s+|\s+$/g, '');
+    return this.replace(/^\s+|\s+$/g, '');
 });
 
 String.method('left', function (n){ // VB like
-	if (n <= 0){
-		return "";
-	}else if (n > String(this).length){
-		return this;
-	}else{
-		return String(this).substring(0,n);
-	}
+    if (n <= 0){
+        return "";
+    }else if (n > String(this).length){
+        return this;
+    }else{
+        return String(this).substring(0,n);
+    }
 });
 
 String.method('right', function (n){ // VB like
-	if (n <= 0){
-		return "";
-	}else if (n > String(this).length){
-		return this;
-	}else{
-		var iLen = String(this).length;
-		return String(this).substring(iLen, iLen - n);
-	}
+    if (n <= 0){
+        return "";
+    }else if (n > String(this).length){
+        return this;
+    }else{
+        var iLen = String(this).length;
+        return String(this).substring(iLen, iLen - n);
+    }
 });
 
 String.method('reverse', function(){ // Just cool
-	splitext = this.split("");
-	revertext = splitext.reverse();
-	reversed = revertext.join("");
-	return reversed;
+    splitext = this.split("");
+    revertext = splitext.reverse();
+    reversed = revertext.join("");
+    return reversed;
 });
 
 String.method('capitalize', function(){ // Pythonic
@@ -66,53 +66,60 @@ String.method('repeat', function(num){ // Just cool
 }); 
 
 String.method('center', function(width, fillchar){ // Pythonic
-	if(width === undefined || width <= this.length){
-		return this;
-	}		
-	if(fillchar === undefined){
-		fillchar = ' ';
-	}
-	width = width - this.length;
-	return fillchar.repeat(width - parseInt(width/2,10)) + this + fillchar.repeat(parseInt(width/2,10)) ;	
+    if(width === undefined || width <= this.length){
+        return this;
+    }        
+    if(fillchar === undefined){
+        fillchar = ' ';
+    }
+    width = width - this.length;
+    return fillchar.repeat(width - parseInt(width/2,10)) + this + fillchar.repeat(parseInt(width/2,10)) ;    
 });
 
 String.method('count', function(sub, start, end){
-	if(sub === undefined || sub == ""){
-		return this.length + 1;
-	}
-	if(start === undefined){
-		start = 0;
-	}
-	if(end === undefined){
-		end = this.length; 
-	}			
-	var n = 0; 
-	var pos = 0; 
-	var str = this.slice(start, end);	
-	while(true){ 
-		pos=str.indexOf(sub,pos); 
-		if(pos!=-1){ n++; pos+=sub.length;} 
-		else{break;} 
-	} 
-	return(n); 	
+    if(sub === undefined || sub == ""){
+        return this.length + 1;
+    }
+    if(start === undefined){
+        start = 0;
+    }
+    if(end === undefined){
+        end = this.length; 
+    }            
+    var n = 0; 
+    var pos = 0; 
+    var str = this.slice(start, end);    
+    while(true){ 
+        pos=str.indexOf(sub,pos); 
+        if(pos!=-1){ n++; pos+=sub.length;} 
+        else{break;} 
+    } 
+    return(n);     
 });
 
 String.method('endswith', function(suffix, start, end){
-	if(suffix === undefined || suffix == ""){
-		return true;
-	}
-	if(start === undefined){
-		start = 0;
-	}
-	if(end === undefined){
-		end = this.length; 
-	}		
-	var str = this.slice(start, end);	
-	return str.indexOf(suffix, str.length - suffix.length) !== -1;	
+    if(suffix === undefined || suffix == ""){
+        return true;
+    }
+    if(start === undefined){
+        start = 0;
+    }
+    if(end === undefined){
+        end = this.length; 
+    }        
+    var str = this.slice(start, end);    
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;    
+});
+
+String.method('expandtabs', function(tabsize){
+    if(tabsize === undefined){
+		tabsize = 8;
+	}	
+	return this.replace(String.fromCharCode(9), 'p'.repeat(tabsize));	
 });
 
 /*
-'expandtabs', 'find', 'format', 'index', 'isalnum', 
+'find', 'format', 'index', 'isalnum', 
 'isalpha', 'isdigit', 'islower', 'isspace', 'istitle', 'isupper',
 'join', 'ljust', 'lower', 'lstrip', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 
 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
